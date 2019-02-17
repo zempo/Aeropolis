7773e15051105324c6bdcaa359f0ef34b467e577
Solomon Zelenko <zelenkosolomon@gmail.com>
2019-02-17 00:14:43 -0700

    update focusing

--- docs/main.js
+++ docs/main.js
@@ -504,7 +504,7 @@ function showWiki() {
     $("a[href|='#results2']").css("border-bottom", "3px solid #010414");
 
     $("a[href|='#results2']").css("opacity", "1");
-    $(".showFullWiki").focus();
+    $("a[href|='#results2']").focus();
 
     $("a[href|='#results2']")
       .siblings("a[href|='#results3']")
@@ -551,7 +551,7 @@ function showNews() {
     // visit news tab, style tabs accordingly
     $("a[href|='#results3']").css("border-bottom", "3px solid #010414");
     $("a[href|='#results3']").css("opacity", "1");
-    $(".showFullNews").focus();
+    $("a[href|='#results3']").focus();
 
     $("a[href|='#results3']")
       .siblings("a[href|='#results2']")
